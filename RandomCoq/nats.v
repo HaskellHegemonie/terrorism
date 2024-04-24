6454514f1d09393eb3360dc1a315ded34557e7f4
HaskellHegemonie <znerol@mail.de>
2024-04-24 23:46:12 +0200

    Commutativity on (*).

--- RandomCoq/nats.v
+++ RandomCoq/nats.v
@@ -33,9 +33,20 @@ Fixpoint exp (a b : nat) : nat :=
 
 Compute exp 2 8.
 
+
+Fixpoint eqb (a b :nat) : bool :=
+  match a, b with
+  | O, O => true
+  | O, _ => false
+  | _, O => false
+  | S n, S m => eqb n m
+  end.
+
+
 Notation "x + y" := (plus x y) (at level 50, left associativity) : nat_scope.
 Notation "x - y" := (minus x y) (at level 50, left associativity) : nat_scope.
 Notation "x * y" := (mult x y) (at level 40, left associativity) : nat_scope.
+Notation "x =? y" := (eqb x y) (at level 40, left associativity) : nat_scope.
 
 Theorem a: ∀ a b : nat, a = b → a + a = b + b.
 Proof.
@@ -119,3 +130,82 @@ Proof.
   - simpl. rewrite -> IHn'. reflexivity.
 Qed.
 
+
+Theorem eqb_refl : ∀ n : nat, (n =? n) = true.
+Proof.
+
+  induction n as [| n' IHn'].
+  - reflexivity.
+  - simpl. rewrite -> IHn'. reflexivity.
+Qed.
+
+
+Theorem mult_0_plus' : ∀ n m : nat, (n + 0 + 0) * m =  n * m.
+Proof.
+  intros n m.
+  assert ( H: n + 0 + 0 = n ).
+  { rewrite plus_commutative. rewrite zero_identity_l. rewrite -> plus_commutative. rewrite zero_identity_l. reflexivity. }
+  rewrite -> H.
+  reflexivity.
+Qed.
+
+Theorem plus_rearrange_firsttry : ∀ n m p q : nat, (n + m)+ (p + q) = (m + n) + (p + q).
+Proof.
+  intros n m p q.
+  (* rewrite -> plus_commutative. *) (* doesn't work. can prove a lemma with vars in scope {-# LANGUAGE ScopedTypeVariables #-} *)
+  assert (H: n + m = m + n). { rewrite -> plus_commutative. reflexivity. }
+  rewrite -> H.
+  reflexivity.
+Qed.
+
+ (* all of following to prove commutativity of * may be possible in fewer steps (maybe ~simpl.~ does more for you?) *)
+
+Theorem mul_zero_l : ∀ x : nat, 0 * x = 0.
+Proof.
+  intros x.
+  induction x as [|x' IHx'].
+  - reflexivity.
+  -  simpl. reflexivity.
+Qed.
+
+Theorem mul_zero_r : ∀ x : nat, x * 0 = 0.
+Proof.
+  intros x.
+  induction x as [|x' IHx'].
+  - reflexivity.
+  - simpl. rewrite -> IHx'. reflexivity.
+Qed.
+
+(* following 2 Theorems may be shortened with ~Theorem add_shuffle3 : ∀ n m p : nat, n + (m + p) = m + (n + p).~ *)
+Theorem comm_3 : ∀ x y z : nat, x + (y + z) = x + (z + y).
+Proof.
+  intros x y z.
+  assert (G: y + z = z + y).
+  { rewrite -> plus_commutative. reflexivity. }
+  rewrite G.
+  reflexivity.
+Qed.
+
+Theorem brace_dont_matter : ∀ x y z : nat, x + (y + z) = x + y + z.
+Proof.
+  intros x y z.
+  induction x as [|x' IHx'].
+  - simpl. reflexivity.
+  - simpl. rewrite -> IHx'. reflexivity.
+Qed.
+
+Theorem destributive_r : ∀ a b : nat, a * S b = a + a * b.
+Proof.
+  intros a b.
+  induction a as [|a' IHa'].
+  - rewrite -> mul_zero_l. rewrite -> mul_zero_l. simpl. reflexivity.
+  - simpl. rewrite -> IHa'. rewrite -> plus_commutative. rewrite comm_3. rewrite -> brace_dont_matter. reflexivity.
+Qed.
+
+Theorem mul_comm : ∀ a b : nat, a * b = b * a.
+Proof.
+  intros a b.
+  induction a as [|a' IHa'].
+  - rewrite -> mul_zero_l. rewrite -> mul_zero_r. reflexivity.
+  - simpl. rewrite ->IHa'.  rewrite -> destributive_r. reflexivity.
+Qed.
